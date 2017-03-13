ccd9cfe98f3680f21da8a30a27d85e50154ee0a6
Nick <vioc.a@mail.ru>
2017-03-13 20:35:12 +0600

    Classes of sql tables

--- src/main/java/web_service/flight_objects/Flight.java
+++ src/main/java/web_service/flight_objects/Flight.java
@@ -1,7 +1,76 @@
 package web_service.flight_objects;
 
+import web_service.flight_dir_object.Aircraft;
+import web_service.flight_dir_object.City;
+
+import java.util.Calendar;
+
 /**
  * Created by Dell on 13.03.2017.
  */
 public class Flight {
+
+    private String code;
+    private Calendar flightDate;
+    private Calendar flightTime;
+    private Aircraft aircraft;
+    private long duration;
+    private City cityFrom;
+    private City cityTo;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Calendar getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(Calendar flightDate) {
+        this.flightDate = flightDate;
+    }
+
+    public Calendar getFlightTime() {
+        return flightTime;
+    }
+
+    public void setFlightTime(Calendar flightTime) {
+        this.flightTime = flightTime;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(Aircraft aircraft) {
+        this.aircraft = aircraft;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public City getCityFrom() {
+        return cityFrom;
+    }
+
+    public void setCityFrom(City cityFrom) {
+        this.cityFrom = cityFrom;
+    }
+
+    public City getCityTo() {
+        return cityTo;
+    }
+
+    public void setCityTo(City cityTo) {
+        this.cityTo = cityTo;
+    }
 }
--- src/main/java/web_service/flight_objects/Passenger.java
+++ src/main/java/web_service/flight_objects/Passenger.java
@@ -4,4 +4,59 @@ package web_service.flight_objects;
  * Created by Dell on 13.03.2017.
  */
 public class Passenger {
+
+    private String givenName;
+    private String middleName;
+    private String familyName;
+    private String documentNumber;
+    private String email;
+    private String phone;
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
